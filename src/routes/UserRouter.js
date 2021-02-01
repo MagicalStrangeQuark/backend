2d2e0f117bbe35a9945e57165b97210a17b6f031
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-02-01 14:03:58 -0300

    UserRouter.js

---
+++ src/routes/UserRouter.js
@@ -0,0 +1,15 @@
+const UserController = require('../User/UserController');
+
+const Router = require('express').Router();
+
+const User = new UserController();
+
+Router.get('/', (request, response) => {
+    return User.index(request, response);
+});
+
+Router.get('/:id', (request, response) => {
+    return User.show(request, response);
+});
+
+module.exports = Router;
