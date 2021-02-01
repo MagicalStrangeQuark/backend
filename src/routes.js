a640393bcb255d88ba43d78f75511086efb3db85
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-02-01 14:13:57 -0300

    routes.js

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+
+const Router = require('express').Router();
+
+Router.use('/', (request, response, next) => {
+    console.log(new Date());
+
+    next();
+});
+
+Router.get('/', (request, response) => {
+    response.status(200).json({});
+});
+
+/**
+ * How To Modularize Routes With The Express Router
+ * 
+ * @link https://medium.com/@catherinelau/how-to-modularize-routes-with-the-express-router-5ce46f9bb2bd
+ * 
+ * @author Cat Lau
+ */
+const UserRouter = require('./User/UserRouter');
+
+Router.use('/users', UserRouter);
+
+module.exports = Router;
